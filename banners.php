490e3d9f73d547dcbaf510076de088c3550c6b8f
Mike Lee <inbox@mikelee.me.uk>
2025-06-11 18:57:27 +0100

    Initial commit

---
+++ banners.php
@@ -0,0 +1,1 @@
+<li><a href="https://www.criticalchicken.com" target="_blank" rel="external nofollow"><img src="banners/criticalchicken.png" alt="Critical Chicken" height="32" width="88"></a></li>
---
+++ monochrome.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en-GB">
+
+<!--
+Copyright (C) Critical Chicken. All rights reserved.
+Critical Chicken, the Critical Chicken logo and wordmark, and #ForTheGaymers are trademarks of Critical Chicken.
+All other trademarks referred to are trademarks of their respective owners.
+-->
+
+<head>
+<meta charset="utf-8">
+<meta name="robots" content="noindex">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Sofia+Sans:wght@600..800&display=swap" rel="stylesheet">
+<style type="text/css">a,abbr,acronym,address,applet,article,aside,audio,b,big,blockquote,body,canvas,caption,center,cite,code,dd,del,details,dfn,div,dl,dt,em,embed,fieldset,figcaption,figure,footer,form,h1,h2,h3,h4,h5,h6,header,hgroup,html,i,iframe,img,ins,kbd,label,legend,li,mark,menu,nav,object,ol,output,p,pre,q,ruby,s,samp,section,small,span,strike,strong,sub,summary,sup,table,tbody,td,tfoot,th,thead,time,tr,tt,u,ul,var,video{margin:0;padding:0;border:0;font-size:100%;font:inherit;vertical-align:baseline}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}body{line-height:1}ol,ul{list-style:none}blockquote,q{quotes:none}blockquote:after,blockquote:before,q:after,q:before{content:'';content:none}table{border-collapse:collapse;border-spacing:0}#branding,#wrapper{top:0;display:block}#branding,body{background-color:#000}#branding a,a.join{opacity:1}#ticker,#wrapper{left:16px;position:absolute}#branding,#ticker,#wrapper{position:absolute}body{font-family:"Sofia Sans",sans-serif;font-weight:800;font-size:12px;line-height:14px;color:#000;text-transform:uppercase;font-style:normal}a{transition:.12s ease-in-out}#wrapper{width:calc(100% - 110px);height:62px;margin:0;padding:2px 0 0 94px;z-index:1;background-image:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAABAAQMAAADh348DAAAABlBMVEX///9xcHB/y9nTAAAADUlEQVR42mMgBBpoCwGAvBgBGYhx6QAAAABJRU5ErkJggg==);background-size:1px 64px;background-position:0 bottom;background-repeat:repeat-x}#branding{width:80px;height:56px;left:0;z-index:3;background-size:32px 1px;background-position:right top;background-repeat:repeat-y;padding:8px 0 0 8px;transform:skew(-26.6deg)}#branding svg{filter:invert(1)}#branding a,#branding a svg{transform:skew(26.6deg)}span.wide{font-weight:600}a.webring{display:none}a.join{float:right;margin-right:8px;color:#000;text-decoration:none}#branding a:hover,a.join:hover,a.webring:hover{opacity:.6}#ticker{width:calc(100% - 16px);height:48px;top:16px;z-index:2}#ticker-inner a{margin-top:8px;display:block;position:relative;border-radius:4px;width:88px;height:32px;margin-bottom:8px}#ticker-inner img{box-shadow:0 4px 8px rgba(0,0,0,.24);border-radius:4px}@media (hover:none){#branding a:hover,a.join:hover{opacity:1}}@media (max-width:492px){span.medium{font-weight:600}span.wide{display:none}}@media (max-width:375px){span.medium,span.wide{display:none}}@media (max-width:306px){span.medium,span.today,span.wide{display:none}}@media (max-width:269px){a.join,span.medium,span.thin,span.wide{display:none}a.webring{margin-right:8px;color:#000;text-decoration:none;opacity:1;display:inline}}@media (max-width:229px){a.webring span{display:none}}@media (prefers-color-scheme:dark){#branding,body{background-color:#fff}a.join,a.webring,body{color:#fff}#wrapper{background-image:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAABAAQMAAADh348DAAAABlBMVEUAAACOj4+P0JVwAAAADUlEQVR42mMgBBpoCwGAvBgBGYhx6QAAAABJRU5ErkJggg==)}#branding svg{filter:invert(0)}}</style>
+<title>Gaymers&plus; Webring</title>
+</head>
+<body>
+<nav id="branding">
+<a href="https://www.criticalchicken.com/webring" target="_blank" rel="help" title="Gaymers&plus; Webring"><svg clip-rule="evenodd"fill-rule="evenodd"height="48"image-rendering="optimizeQuality"shape-rendering="geometricPrecision"text-rendering="geometricPrecision"version="1.1"viewBox="0 0 74 48"width="74"xmlns="http://www.w3.org/2000/svg"><g transform="matrix(.051118 0 0 .051296 -3.6743 -2.3853)"><path d="m1105.5 46.5h197v217h217v177h-217v215h-197v-215h-214v541h-176c0.332-13.017-1e-3 -26.017-1-39-88.359 38.571-180.03 49.071-275 31.5-100.41-20.936-184.91-69.103-253.5-144.5-106.4-126.29-138.4-269.63-96-430 49.928-150.26 149.43-252.1 298.5-305.5 152.77-44.653 292.77-19.653 420 75 23.322 17.646 45.156 37.146 65.5 58.5 0.667 0.667 0.667 1.333 0 2-43.35 40.184-86.85 80.184-130.5 120-33.708-36.199-73.374-64.032-119-83.5-99.434-32.151-190.43-16.484-273 47-77.12 65.636-110.62 149.14-100.5 250.5 18.448 109.1 78.948 184.27 181.5 225.5 81.749 28.658 160.08 21.325 235-22 16.798-9.814 32.631-20.981 47.5-33.5 0.5-39.332 0.667-78.665 0.5-118-64.003 0.333-128 0-192-1 29.105-57.876 57.938-115.88 86.5-174 93.429-0.999 186.93-1.332 280.5-1v-176h215v-217z"opacity=".994"/></g></svg></a>
+</nav>
+<div id="wrapper" role="contentinfo">
+<a href="https://www.criticalchicken.com/webring" target="_blank" rel="help" class="webring">G<span>aymers</span>&plus;&nbsp;Webring</a>
+<span class="wide">This&nbsp;site&nbsp;is&nbsp;proudly&nbsp;</span><span class="medium">part&nbsp;of&nbsp;the&nbsp;</span><span class="thin">Gaymers&plus;&nbsp;Webring</span>
+<a href="https://www.criticalchicken.com/webring" target="_blank" rel="help" class="join">Join<span class="today"> today</span></a>
+</div>
+<main id="ticker">
+<ul id="ticker-inner">
+<?php include "banners.php"; ?>
+</ul>
+</main>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script>!window.jQuery && document.write('<script src="jquery-3.4.1.min.js"><\/script>')</script>
+<script src="jquery.webticker.min.js"></script>
+<script>$((function(){var e=$("#ticker-inner"),r=e.children("li");r.sort((function(){return Math.random()-.5})),e.empty().append(r),e.webTicker({height:"48px",duplicate:!0,rssfrequency:0,startEmpty:!1,hoverpause:!0,speed:48})}));</script>
+</body>
+</html>
